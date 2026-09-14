e4bc7fda67208a61e5781ee388a6c49275ed7245
Alumno <Alumno@PC225>
2026-09-14 11:00:30 -0300

    Agregar archivos de proyecto.

---
+++ pryOlariagaSP2/frmVentaTicket.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace pryOlariagaSP2
+{
+    partial class frmVentaTicket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.dtmFecha = new System.Windows.Forms.DateTimePicker();
+            this.cmbTickets = new System.Windows.Forms.ComboBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lstRegistro = new System.Windows.Forms.ListBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(121, 50);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(163, 20);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // dtmFecha
+            // 
+            this.dtmFecha.Location = new System.Drawing.Point(101, 12);
+            this.dtmFecha.Name = "dtmFecha";
+            this.dtmFecha.Size = new System.Drawing.Size(213, 20);
+            this.dtmFecha.TabIndex = 1;
+            // 
+            // cmbTickets
+            // 
+            this.cmbTickets.FormattingEnabled = true;
+            this.cmbTickets.Items.AddRange(new object[] {
+            "- Básico",
+            "- Estándar",
+            "-Premium"});
+            this.cmbTickets.Location = new System.Drawing.Point(121, 90);
+            this.cmbTickets.Name = "cmbTickets";
+            this.cmbTickets.Size = new System.Drawing.Size(163, 21);
+            this.cmbTickets.TabIndex = 2;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(234, 133);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(80, 24);
+            this.btnRegistrar.TabIndex = 3;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lstRegistro
+            // 
+            this.lstRegistro.FormattingEnabled = true;
+            this.lstRegistro.Location = new System.Drawing.Point(23, 163);
+            this.lstRegistro.Name = "lstRegistro";
+            this.lstRegistro.Size = new System.Drawing.Size(291, 82);
+            this.lstRegistro.TabIndex = 4;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(47, 53);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(67, 13);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nº de Ticket";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(32, 93);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(82, 13);
+            this.lblTipo.TabIndex = 6;
+            this.lblTipo.Text = "Tipo de Ticket: ";
+            // 
+            // frmVentaTicket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 318);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lstRegistro);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.cmbTickets);
+            this.Controls.Add(this.dtmFecha);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "frmVentaTicket";
+            this.Text = "frmVentaTicket";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.DateTimePicker dtmFecha;
+        private System.Windows.Forms.ComboBox cmbTickets;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListBox lstRegistro;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTipo;
+    }
+}
---
+++ pryOlariagaSP2/frmVentaTicket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryOlariagaSP2
+{
+    public partial class frmVentaTicket : Form
+    {
+        public frmVentaTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if ( txtNumero.Text == "")
+
+            MessageBox.Show("Debe ingresar un número de ticket", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+              if (cmbTickets.SelectedIndex == -1)
+                MessageBox.Show("Debe seleccionar un ticket", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+              else
+              {
+                // Agregar el ticket a la lista
+                lstRegistro.Items.Add(cmbTickets.SelectedItem.ToString());
+                cmbTickets.SelectedIndex = -1;
+              }
+            }
+
+
+        }
+    }
+}
